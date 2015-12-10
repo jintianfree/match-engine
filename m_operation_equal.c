7420213ccbc2f40caa8fb9657011d2f552aaf0fd
andrew <andrew.shen@foxmail.com>
2015-12-10 19:02:23 +0800

    update m_operation_equal.c

--- m_operation_equal.c
+++ m_operation_equal.c
@@ -5,65 +5,96 @@ struct equal_operator_option {
 	int case_sensitive;
 };
 
-/* equal(var_name[][]: value) */
+/* "AB EF CD"  -> {AB,EF,CD} */
+int bytes_string_2_bytes(char *str, s_len, uint8_t bytes[], int b_len)
+{
+	char *p = NULL;
+	
+	for(p = strtok(str, ' \t'); p != NULL; p = strtok(NULL, ' \t')) {
+		strtol(p, NULL, 16);
+		if(errno == ERANGE)
+	}
+	
+err:
+	return -1;
+}
+
+/* TODO: do not support option now! equal(var_name[][]: value) */
 int m_op_equal_parse(const char *s, struct m_operation *operation, struct m_variable_list *head)
 {
+	int eno = 0;
+	int len = 0;
+	char *p = NULL;
 	struct m_variable *var = NULL;
 
 	len = strlen(s);
+
 	p = malloc(len + 1);	/* +1 for \0 */
 	if(p == NULL) {
+		eno = -1;
 		goto err;
 	}
 
 	strcpy(p, s);
 	p[len] = 0;
 
+	/* equal(var_name:value) */
+	char *op = strtok(p, "(:)");
+	if(op == NULL) {
+		eno = -2;
+		goto err;
+	}
 
-	char *op = strtok(p, "(:)";
-	char *var_name = strtok(NULL, "(:)";
-	char *value = strtok(NULL, "(:)";
+	char *var_name = strtok(NULL, "(:)");
+	if(var_name == NULL) {
+		eno = -2;
+		goto err;
+	}
 
-	var = name_2_var(var_name);
+	char *value = strtok(NULL, "(:)");
+	if(value == NULL) {
+		eno = -2;
+		goto err;
+	}
+
+	if((var = name_2_var(var_name)) == NULL) {
+		eno  = -3;
+		goto err;
+	}
 
 	operation->var = var;
 
 	switch(var->type) {
-	case MT_UINT8:
-		operation->func = m_op_equal_uint8;
-		operation->value = atoi(value);
-		operation->value_len = 1;
-		break;
-	case MT_UINT16:
-		operation->func = m_op_equal_uint16;
-		operation->value = atoi(value);
-		operation->value_len = 2;
-		break;
 	case MT_UINT32:
 		operation->func = m_op_equal_uint32;
 		operation->value = atoi(value);
 		operation->value_len = 4;
+		break;
+	case MT_STRING:
+		operation->func = m_op_equal_string;
+		operation->value = atoi(value);
+		operation->value_len = 4;
+		break;
+	case MT_BYTES:
+		operation->func = m_op_equal_bytes;
+		operation->value = atoi(value);
+		operation->value_len = 4;
+		break;
+	default:
+		eno = -4;
+		goto err;
 		break;
 	}
 	
 	operation->operator_option = NULL;
 
 	return 0;
+err:
+	return -1;
 }
 
 void m_op_equal_clean(struct m_operation *operation)
 {
-}
-/* equal great little contain range null */
-
-int m_op_equal_uint8(void *var, int var_len, void *value, int value_len)
-{
-	return (*(uint8_t *)var == *(uint8_t *)value);
-}
-
-int m_op_equal_uint16(void *var, int var_len, void *value, int value_len)
-{
-	return (*(uint16_t *)var == *(uint16_t *)value);
 }
 
 int m_op_equal_uint32(void *var, int var_len, void *value, int value_len)
@@ -71,30 +102,6 @@ int m_op_equal_uint32(void *var, int var_len, void *value, int value_len)
 	return (*(uint32_t *)var == *(uint32_t *)value);
 }
 
-int m_op_equal_uint64(void *var, int var_len, void *value, int value_len)
-{
-	return (*(uint64_t *)var == *(uint64_t *)value);
-}
-
-int m_op_equal_int8(void *var, int var_len, void *value, int value_len)
-{
-	return (*(int8_t *)var == *(int8_t *)value);
-}
-
-int m_op_equal_int16(void *var, int var_len, void *value, int value_len)
-{
-	return (*(int16_t *)var == *(int16_t *)value);
-}
-
-int m_op_equal_int32(void *var, int var_len, void *value, int value_len)
-{
-	return (*(int32_t *)var == *(int32_t *)value);
-}
-
-int m_op_equal_int64(void *var, int var_len, void *value, int value_len)
-{
-	return (*(int64_t *)var == *(int64_t *)value);
-}
 
 int m_op_equal_string(void *var, int var_len, void *value, int value_len)
 {
@@ -114,19 +121,18 @@ int m_op_equal_bytes(void *var, int var_len, void *value, int value_len)
 	return (memcmp(var, value, var_len) == 0);
 }
 
-
 struct m_operation m_op_equal = {
 	.name = "equal",
 	.parse_func = NULL,
 	.op_func = {
-		m_op_equal_uint8,
-		m_op_equal_uint16,
+		NULL,
+		NULL,
 		m_op_equal_uint32,
-		m_op_equal_uint64,
-		m_op_equal_int8,
-		m_op_equal_int16,
-		m_op_equal_int32,
-		m_op_equal_int64,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
 		NULL,
 		NULL,
 		NULL,
